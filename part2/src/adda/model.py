20345ff57ed33bbfdf29b81a3eca693923d12fac
demi guo <xtt@megvii.com>
2017-12-16 10:46:38 +0800

    add outputt grad

--- part2/src/adda/model.py
+++ part2/src/adda/model.py
@@ -36,6 +36,14 @@ class myFNN(torch.nn.Module):
                 params.append(param)
         return params
     
+    def output_grad(self):
+        print "------- myFNN GRADIENTS (start) --------"
+        print self.MLP[0].weight.grad
+        print self.MLP[2].weight.grad
+        print self.MLP[4].weight.grad
+        print "------- myFNN GRADIENTS (end) --------"
+
+
     def forward(self, input):
         return self.MLP(input)
 
@@ -161,6 +169,12 @@ class myLSTM(torch.nn.Module):
                 params.append(param)
         return params
 
+    def output_grad(self):
+        print "$$$$$$$$$$$ LSTM gradients (start) $$$$$$$$$$$$"
+        print self.lstm.weight_ih_l0.grad
+        print self.lstm.weight_hh_l0.grad
+        print "all weights=", self.lstm._all_weights
+        print "$$$$$$$$$$$ LSTM gradients (end) $$$$$$$$$$$$"
     """ Sort text data in nonincreasing order by length, and return new text, text_len """
     def preprocess(self, text, text_len):
         text = text.cpu()
